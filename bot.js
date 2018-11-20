8e6d604141103dfd11354161c864ac52bad39996
hassangamesdiscord <44375561+hassangamesdiscord@users.noreply.github.com>
2018-11-20 12:32:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -500,7 +500,7 @@ client.on('guildMemberAdd', member => {
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const logChannel = member.guild.channels.find(channel => channel.name === "chat");
-    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+    logChannel.send(`${member} **Invited by:** <@${inviter.id}>`);
   });
 });
 
