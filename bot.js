d497bcfdb428c275d533652a09acf9fb09cc2cf2
hassangamesdiscord <44375561+hassangamesdiscord@users.noreply.github.com>
2018-11-21 00:36:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,21 +12,6 @@ client.on('ready', () => {
 
 
 
-const moment = require("moment")
-client.on("guildMemberAdd", m => {
-    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
-        m.ban();
-    };
-    function parseDate(str) {
-        var mdy = str.split('/');
-        return new Date(mdy[2], mdy[0]-1, mdy[1]);
-    };
-   
-    function datediff(first, second) {
-        return Math.round((second-first)/(1000*60*60*24));
-    };
-});
-
 
 
 
@@ -560,6 +545,23 @@ client.on('message',function(message) {
     message.delete()
     message.reply("ممنوع السب"); 
   }
+});
+
+
+
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+   
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
 });
 
 
