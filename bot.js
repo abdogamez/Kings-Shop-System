565935a9d3537bc163b2bd7e91661e6304a494b3
hassangamesdiscord <44375561+hassangamesdiscord@users.noreply.github.com>
2018-11-21 12:54:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,36 +11,6 @@ client.on('ready', () => {
 ////////////////تجربه اكواد
 
 
-client.on("message", async message => {
-            if(!message.channel.guild) return;
-            var prefix = "%";
-        if(message.content.startsWith(prefix + 'invites')) {
-        var nul = 0
-        var guild = message.guild
-        await guild.fetchInvites()
-            .then(invites => {
-             invites.forEach(invite => {
-                if (invite.inviter === message.author) {
-                     nul+=invite.uses
-                    }
-                });
-            });
-          if (nul > 0) {
-              console.log(`\n${message.author.tag} has ${nul} invites in ${guild.name}\n`)
-              var embed = new Discord.RichEmbed()
-                  .setColor("#000000")
-                    .addField(`${message.author.username}`, `لقد قمت بدعوة **${nul}** شخص`)
-                          message.channel.send({ embed: embed });
-                      return;
-                    } else {
-                       var embed = new Discord.RichEmbed()
-                        .setColor("#000000")
-                        .addField(`${message.author.username}`, `لم تقم بدعوة أي شخص لهذة السيرفر`)
-
-                       message.channel.send({ embed: embed });
-                        return;
-                    }
-        } 
 
 
  /////////////////////////////////////////////وامر عامه
