99b00dbcdb020c8f3dd50be2dc190936b4425648
hassangamesdiscord <44375561+hassangamesdiscord@users.noreply.github.com>
2018-11-22 19:24:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,19 +11,6 @@ client.on('ready', () => {
 ////////////////تجربه اكواد
 
 
-client.on("message", message => {
- 
-            if (message.content.startsWith(prefix + "bcc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
- message.delete();
-};    
-});
 
 
 
