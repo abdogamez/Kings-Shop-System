6b16046ece8e74d03aa76266359864e7a7351395
ezookik <45100686+bankroom@users.noreply.github.com>
2018-12-07 11:57:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`*help | By ezoo_kik`,"http://twitch.tv/S-F")
+client.user.setGame(`*help | By ezoo_kik_yt`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
